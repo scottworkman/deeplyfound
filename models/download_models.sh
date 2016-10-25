7abaa6170760a2e7f9252223c106a924993f4bd8
Scott Workman <scottworkman@users.noreply.github.com>
2016-10-25 18:40:44 -0400

    update download link

--- models/download_models.sh
+++ models/download_models.sh
@@ -5,7 +5,7 @@
 
 echo "Downloading..."
 
-wget 'https://doc-0g-5g-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/720qsepsptd6en0r524ks3685untmfg8/1472248800000/05018003348500916163/*/0BzEcTtT1A2ILUUdLaFRWcVQ0SUU?e=download' -O 'deeplyfound.zip'
+wget http://amos.csr.uky.edu/modelzoo/deeplyfound/deeplyfound.zip
 
 echo "Unzipping..."
 
